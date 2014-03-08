099241fd0b0ce46e6f10198633343938882e573a
Alexander Erben <a_erben@outlook.com>
2014-03-08 16:03:37 +0100

    Introduce solution for #49.

--- src/fourclojure_solutions/core.clj
+++ src/fourclojure_solutions/core.clj
@@ -1,13 +1,20 @@
 (ns fourclojure-solutions.core)
 
-(defn interposition [value list]
-  (flatten (reduce (fn [lst val] [lst value val]) list)))
+(defn interposition_40 "Solution 40. Interpose alternative."
+  [value list]
+  (flatten
+    (reduce
+      (fn [lst val] [lst value val]) list)))
 
-(defn dropN [lst n]
-  (reverse(loop [accum '() curlist lst curN n]
+(defn dropN_41 "Solution 41. Drop nth element."
+  [lst n]
+  (reverse (loop [accum '() curlist lst curN n]
              (cond
                (empty? curlist) accum
                (= 1 curN) (recur accum (rest curlist) n)
-               :else (recur (conj accum (first curlist)) (rest curlist) (dec curN))
-               )
-             )))
+               :else (recur (conj accum (first curlist)) (rest curlist) (dec curN))))))
+
+(defn split49 "Solution 49. Split at n."
+  [n lst]
+  (list
+    (take n lst) (drop n lst)))
--- test/fourclojure_solutions/test/core.clj
+++ test/fourclojure_solutions/test/core.clj
@@ -3,14 +3,17 @@
   (:use [clojure.test]))
 
 
-(deftest interpositionTest
-  (is (= (interposition 0 [1 2 3]) [1 0 2 0 3]
+(deftest interposition40Test
+  (is (= (interposition_40 0 [1 2 3]) [1 0 2 0 3]
          ) "Interpose of 0 [1 2 3] should be [1 0 2 0 3]."))
 
+(deftest dropn41Test
+  (is (= (dropN_41 [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8]))
+  (is (= (dropN_41 [:a :b :c :d :e :f] 2) [:a :c :e])))
 
-
-(deftest dropnTest
-  (is (= (dropN [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8]))
-  (is (= (dropN [:a :b :c :d :e :f] 2) [:a :c :e])))
+(deftest split49Test
+  (is (= (split49 3 [1 2 3 4 5 6]) [[1 2 3] [4 5 6]]))
+  (is (= (split49 1 [:a :b :c :d]) [[:a] [:b :c :d]]))
+  (is (= (split49 2 [[1 2] [3 4] [5 6]]) [[[1 2] [3 4]] [[5 6]]])))
 
 (run-tests)
