421c9d72016e1cee1f60172ac1e6bcbb667c0e32
Alexander Erben <a_erben@outlook.com>
2014-03-08 20:40:33 +0100

    Introduce solution to 166.

--- src/fourclojure_solutions/fourclojure.clj
+++ src/fourclojure_solutions/fourclojure.clj
@@ -1,25 +1,29 @@
 (ns fourclojure-solutions.fourclojure)
 
-(defn flat_28 "Flatten a sequence"
+(defn flat_28
+  "Flatten a sequence"
   [toFlat]
   (filter
     (complement sequential?)
     (tree-seq sequential? seq toFlat)))
 
-(defn repeater_33 "Solution 33. Repeat each element of a sequence n times and concatenate"
+(defn repeater_33
+  "Solution 33. Repeat each element of a sequence n times and concatenate"
   [seq n]
   (mapcat identity
           (map
             #(repeat n %1)
             seq)))
 
-(defn interposition_40 "Solution 40. Interpose alternative."
+(defn interposition_40
+  "Solution 40. Interpose alternative."
   [value list]
   (flatten
     (reduce
       (fn [lst val] [lst value val]) list)))
 
-(defn dropN_41 "Solution 41. Drop nth element."
+(defn dropN_41
+  "Solution 41. Drop nth element."
   [lst n]
   (reverse (loop [accum '() curlist lst curN n]
              (cond
@@ -27,19 +31,26 @@
                (= 1 curN) (recur accum (rest curlist) n)
                :else (recur (conj accum (first curlist)) (rest curlist) (dec curN))))))
 
-(defn factorial_42 "Solution 42. Calculate the factorial." [n]
+(defn factorial_42
+  "Solution 42. Calculate the factorial." [n]
   (reduce * (range 1 (inc n))))
 
-(defn split_49 "Solution 49. Split at n."
+(defn split_49
+  "Solution 49. Split at n."
   [n lst]
   (list
     (take n lst) (drop n lst)))
 
-
-
-(defn truthy_83 "Solution 83. Variadic, takes booleans. Return if some are true and some are false."
+(defn truthy_83
+  "Solution 83. Variadic, takes booleans. Return if some are true and some are false."
   [& bools]
   (not (nil?
          (and
            (seq (drop-while true? bools))
            (seq (drop-while false? bools))))))
+
+(defn comparison_166
+  "Solution 166. Takes a lower-than-operator and two operands, returns a keyword signalling the relationship."
+  [ltOp fst snd]
+  (cond (ltOp fst snd) :lt (ltOp snd fst) :gt :else :eq)
+  )
--- test/fourclojure_solutions/test/fourclojureTest.clj
+++ test/fourclojure_solutions/test/fourclojureTest.clj
@@ -5,6 +5,16 @@
 (deftest flatten28Test
   (is (= (flat_28 ["a" ["b"] "c"]) '("a" "b" "c"))))
 
+(comment
+  (deftest compress30Test
+
+    (is (= (apply str (compress_30 "Leeeeeerrroyyy")) "Leroy"))
+    (is (= (compress_30 [1 1 2 3 3 2 2 3]) '(1 2 3 2 3)))
+    (is (= (compress_30 [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2])))))
+
+(deftest repeater33Test
+  (is (= (repeater_33 [:a :b] 4) '(:a :a :a :a :b :b :b :b))))
+
 (deftest interposition40Test
   (is (= (interposition_40 0 [1 2 3]) [1 0 2 0 3]
          ) "Interpose of 0 [1 2 3] should be [1 0 2 0 3]."))
@@ -21,21 +31,24 @@
   (is (= (split_49 1 [:a :b :c :d]) [[:a] [:b :c :d]]))
   (is (= (split_49 2 [[1 2] [3 4] [5 6]]) [[[1 2] [3 4]] [[5 6]]])))
 
-(deftest truthy83Test
-  (is (= false (truthy_83 false false)))
-  (is (= true (truthy_83 true false)))
-  (is (= false (truthy_83 true true true))))
-
-(deftest repeater33Test
-  (is (= (repeater_33 [:a :b] 4) '(:a :a :a :a :b :b :b :b))))
-
-
-
 (comment
   (deftest splitType50Test
     (is (= (set (splitType50 [1 :a 2 :b 3 :c])) #{[1 2 3] [:a :b :c]}))
     (is (= (set (splitType50 [:a "foo" "bar" :b])) #{[:a :b] ["foo" "bar"]}))
     (is (= (set (splitType50 [[1 2] :a [3 4] 5 6 :b])) #{[[1 2] [3 4]] [:a :b] [5 6]})))
   )
+
+(deftest truthy83Test
+  (is (= false (truthy_83 false false)))
+  (is (= true (truthy_83 true false)))
+  (is (= false (truthy_83 true true true))))
+
+(deftest comparison166Test
+  (is (= :gt (comparison_166 < 5 1)))
+  (is (= :eq (comparison_166 (fn [x y] (< (count x) (count y))) "pear" "plum")))
+  (is (= :lt (comparison_166 (fn [x y] (< (mod x 5) (mod y 5))) 21 3)))
+  (is (= :gt (comparison_166 > 0 2))))
+
+
 
 (run-tests)
